2bd827f8c7e4f83694e3b3bd68ee880fb2afed44
thearnabsaha <thearnabsaha201@gmail.com>
2025-01-25 19:14:40 +0530

    version 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk=require("chalk"); 
 const { Command } = require('commander');
 const program = new Command();
 
@@ -9,11 +10,14 @@ program
   .version('1.0.0');
 
 program.command('read')
-  .description('to read all the todos (node index.js read)')
+  .description(chalk.green('to read all the todos (node index.js read)'))
   .action(()=>{
     fs.readFile('todos.json', 'utf8', (err, data) => {
         if (err) {
           console.error('Error reading file:', err);
+          console.log(chalk.green('Reset the Tasklist First'));
+        console.log(chalk.green(`Use this code first : ${chalk.white("npm run todo reset")}`));
+
           return;
         }
         console.log(JSON.parse(data));
@@ -21,12 +25,15 @@ program.command('read')
   })
   
 program.command('add')
-  .description('to add a todo (node index.js add "buy coffee" ) ')
+  .description(chalk.green('to add a todo (node index.js add "buy coffee" ) '))
   .argument('<string>', 'which todo will be added')
   .action(()=>{
     fs.readFile('todos.json', 'utf8', (err, data) => {
       if (err) {
         console.error('Error reading file:', err);
+        console.log(chalk.green('Reset the Tasklist First'));
+        console.log(chalk.green(`Use this code first : ${chalk.white("npm run todo reset")}`));
+
         return;
       }
       todos=JSON.parse(data)
@@ -34,38 +41,40 @@ program.command('add')
         todos.push({task:program.args[1].trim()})
         fs.writeFile('todos.json', `${JSON.stringify(todos)}`, (err) => {
           if (err) throw err;
-          console.log('File written successfully');
+          console.log(chalk.green('Task Added successfully'));
         });
       }else{
-        console.log("Task already exists");
+        console.log(chalk.green("Task already exists"));
       }
     });
   })
 
 program.command('done')
-  .description('to delete a todo (node index.js done "buy coffee" ) ')
+  .description(chalk.green('to delete a todo (node index.js done "buy coffee" ) '))
   .argument('<string>', 'which todo will be added')
   .action(()=>{
     fs.readFile('todos.json', 'utf8', (err, data) => {
       if (err) {
         console.error('Error reading file:', err);
+        console.log(chalk.green('Reset the Tasklist First'));
+        console.log(chalk.green(`Use this code first : ${chalk.white("npm run todo reset")}`));
         return;
       }
       todos=JSON.parse(data)
-      todos=todos.filter(e=>e.task!==program.args[1])
+      todos=todos.filter(e=>e.task!==program.args[1].trim())
       fs.writeFile('todos.json', `${JSON.stringify(todos)}`, (err) => {
         if (err) throw err;
-        console.log('File written successfully');
+        console.log(chalk.green('Task removed successfully'));
       });
     });
   })
 
 program.command('reset')
-  .description('to reset the todolist (node index.js reset) ')
+  .description(chalk.green('to reset the todolist (node index.js reset) '))
   .action(()=>{
       fs.writeFile('todos.json', `[]`, (err) => {
         if (err) throw err;
-        console.log('File written successfully');
+        console.log(chalk.green('TaskList Resets successfully'));
       });
   })
 program.parse();
